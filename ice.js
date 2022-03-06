dcff7b7de29098952b26c82cfa53ff37517c5438
Ann Wanjeri <wannjeri2@gmail.com>
2022-03-07 00:42:04 +0300

    selects html document

--- ice.js
+++ ice.js
@@ -1,18 +1,13 @@
-const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw", "Kofi", "Kwame"]
-const FemNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+"use strict";
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+let subBtn = document.querySelector("#btn");
+let genderChoice = document.querySelectorAll("#gender")
 
 
 
-let subBtn = document.querySelector("btn")
-// const d = new Date()
-const userDate = document.querySelector("#birthdate");
-
-// subBtn.addEventListener("click" => (e) {
-//     e.preventDefault
-// })
-
-
-// const userGender = document.querySelectorAll("#")
 
 
