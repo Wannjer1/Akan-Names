88cd2457c81ff1caaaf1cf6cde3e28ba01fbac77
Ann Wanjeri <wannjeri2@gmail.com>
2022-03-06 22:06:57 +0300

    const variable added

---
+++ ice.js
@@ -0,0 +1,18 @@
+const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw", "Kofi", "Kwame"]
+const FemNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+
+
+let subBtn = document.querySelector("btn")
+// const d = new Date()
+const userDate = document.querySelector("#birthdate");
+
+// subBtn.addEventListener("click" => (e) {
+//     e.preventDefault
+// })
+
+
+// const userGender = document.querySelectorAll("#")
+
+
